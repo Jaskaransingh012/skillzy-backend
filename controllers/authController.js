a136c789a801d89f606cedee394cb1d1d1b6ebb1
Jaskaransingh012 <jaskaransingh_1@outlook.com>
2025-02-25 12:13:28 +0530

    khana time

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,11 +12,9 @@ exports.register = async (req, res) => {
     if (user) return res.status(400).json({ message: 'User already exists' });
   
     // Hash password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
+   
     // Create new user
-    user = new User({ username, email, password: hashedPassword, role });
+    user = new User({ username, email, password, role });
     await user.save();
 
     res.status(201).json({ message: 'User registered successfully' });
